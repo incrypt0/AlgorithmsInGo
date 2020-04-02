a0f7b7a05c7be91a1ed426da065362129815c389
krishna-incrypto <vpkrishnanand@gmail.com>
2020-04-02 19:39:09 +0530

    learn go

---
+++ algorithms/sorting/insertion_sort/insertion_sort.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	arr := [10]int{7, 3, 8, 5, 1, 9}
+	fmt.Println(arr)
+}
---
+++ basics/03arrays/arrays.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var arr1 [4]int
+	var arr2 = [4]int{1, 2, 3, 4}
+	fmt.Println(arr1, arr2)
+}
