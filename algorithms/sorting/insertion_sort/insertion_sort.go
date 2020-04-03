2a4a2018288c50efd6b89b7fabd166e0708d7c21
krishna-incrypto <vpkrishnanand@gmail.com>
2020-04-03 09:53:49 +0530

    insertion sort added

--- algorithms/sorting/insertion_sort/insertion_sort.go
+++ algorithms/sorting/insertion_sort/insertion_sort.go
@@ -1,6 +1,21 @@
 package main
 
-func main() {
-	arr := [10]int{7, 3, 8, 5, 1, 9}
+import "fmt"
 
+func main() {
+	arr := [10]int{7, 3, 8, 5, 1, 9, 2, 4, 0, 6}
+
+	for i := 0; i < len(arr); i++ {
+		fmt.Println(i, arr[i])
+		item1 := arr[i]
+		j := i - 1
+
+		for j >= 0 && item1 < arr[j] {
+
+			arr[j+1] = arr[j]
+			j--
+		}
+		arr[j+1] = item1
+	}
+	fmt.Println(arr)
 }
---
+++ basics/07while_loop/while.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	num := 10
+	for num > 0 {
+		fmt.Println(num)
+		num--
+	}
+}
