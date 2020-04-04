905b77029500d5e508f9e70ccfb4efd8b2291f7f
krishna-incrypto <vpkrishnanand@gmail.com>
2020-04-04 22:41:52 +0530

    selection_sort

--- helloworld.go
+++ helloworld.go
@@ -2,6 +2,10 @@ package main
 
 import "fmt"
 
+func add(num1, num2 float64) float64 { //This is same ass (num1 float4 ,num2 float64)
+	return num1 + num2
+}
 func main() {
-	fmt.Println("Hello,World!")
+	fmt.Println("Hello,World!\nHere is an addition")
+	fmt.Println(add(2.4, 3.2))
 }
