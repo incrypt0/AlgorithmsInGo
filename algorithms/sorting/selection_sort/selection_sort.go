4012491dbb58d2ff3c650108f9dc62df17407e32
krishna-incrypto <vpkrishnanand@gmail.com>
2020-04-05 07:18:35 +0530

    arrays

--- algorithms/sorting/selection_sort/selection_sort.go
+++ algorithms/sorting/selection_sort/selection_sort.go
@@ -1,11 +1,18 @@
 package main
 
+func lowestIndex(arr []int) int {
+	var length, minIndex int = len(arr), 0
+	for i := 0; i < length; i++ {
+		if arr[i] < arr[minIndex] {
+			minIndex = i
+		}
+	}
+	return minIndex
+}
 func main() {
 	arr := [10]int{7, 3, 8, 5, 1, 9, 2, 4, 0, 6}
 	length := len(arr)
-	for i := 0; i <= length; i++ {
-		for j := i + 1; j <= length; i++ {
-
-		}
+	for i := 0; i < length; i++ {
+		lowestIndex(arr[i+1:])
 	}
 }
