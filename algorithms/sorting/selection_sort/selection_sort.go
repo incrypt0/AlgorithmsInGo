8c2fcbe54a30d9521f6d0b195792a44402016e35
krishna-incrypto <vpkrishnanand@gmail.com>
2020-04-05 07:06:38 +0530

    selection sort

--- algorithms/sorting/selection_sort/selection_sort.go
+++ algorithms/sorting/selection_sort/selection_sort.go
@@ -1,8 +1,11 @@
 package main
 
-import "fmt"
-
 func main() {
-	var a = []int{1, 2, 4}
-	fmt.Println(a)
+	arr := [10]int{7, 3, 8, 5, 1, 9, 2, 4, 0, 6}
+	length := len(arr)
+	for i := 0; i <= length; i++ {
+		for j := i + 1; j <= length; i++ {
+
+		}
+	}
 }
