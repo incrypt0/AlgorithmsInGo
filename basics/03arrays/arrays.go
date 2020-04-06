e8357e7a6ee2f8d8274ecd820c794bfab4f1bf94
krishna-incrypto <vpkrishnanand@gmail.com>
2020-04-06 19:03:04 +0530

    cmd args added

--- basics/03arrays/arrays.go
+++ basics/03arrays/arrays.go
@@ -5,9 +5,12 @@ import (
 )
 
 func main() {
-	var arr1 [4]int
-	var arr2 = [4]int{1, 2, 3, 4}
+
 	var arr3 = [5]int{1, 2, 3, 4, 5}
-	fmt.Println(arr1, arr2, arr3)
-	fmt.Println(arr3[:3])
+
+	//Demonstration of range
+	for a, b := range arr3 {
+		fmt.Println(a, b)
+	}
+
 }
---
+++ basics/10command_line_arguments/cmdargs.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	arg1 := os.Args[1]
+	fmt.Println(arg1)
+}
