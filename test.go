37e37a2ebf3c76cfb9184a373f7c32121a030657
krishna-incrypto <vpkrishnanand@gmail.com>
2020-04-07 19:19:10 +0530

    added slices

--- test.go
+++ test.go
@@ -1,10 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	var x float64
-	fmt.Scan(&x)
-	a := int(x)
-	fmt.Println(a)
+	var xtemp int
+	x1 := 0
+	x2 := 1
+	for x := 0; x < 5; x++ {
+		xtemp = x2
+		x2 = x2 + x1
+		x1 = xtemp
+	}
+	fmt.Println(x2)
 }
