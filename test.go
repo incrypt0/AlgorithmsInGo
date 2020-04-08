a70f567e37001edf2786ba83f5fef46beb79580b
krishna-incrypto <vpkrishnanand@gmail.com>
2020-04-08 09:13:04 +0530

    files ioutil added

--- test.go
+++ test.go
@@ -1,28 +1,29 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 )
 
-func sortItBaby(arr []int) []int {
-	for i := 0; i < len(arr); i++ {
-		item1 := arr[i]
-		j := i - 1
-		for j >= 0 && item1 < arr[j] {
-			arr[j+1], arr[j] = arr[j], arr[j+1]
-			j--
-		}
-	}
-	return arr
-}
-
-type P struct {
-	x string
-	y int
+type Box struct {
+	Width  int
+	Height int
+	Color  string
+	Open   bool
 }
 
 func main() {
-	s := make([]int, 0, 3)
-	s = append(s, 100)
-	fmt.Println(len(s), cap(s))
+	// Create an instance of the Box struct.
+	box := Box{
+		Width:  10,
+		Height: 20,
+		Color:  "blue",
+		Open:   false,
+	}
+	// Create JSON from the instance data.
+	// ... Ignore errors.
+	b, _ := json.Marshal(box)
+	// Convert bytes to string.
+	s := string(b)
+	fmt.Println(s)
 }
