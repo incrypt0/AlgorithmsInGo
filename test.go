450f26660d6180a781aebc53b54fd8bd04f97c08
krishna-incrypto <vpkrishnanand@gmail.com>
2020-04-12 12:13:16 +0530

    dynamic argument implementation

--- test.go
+++ test.go
@@ -1,11 +1,77 @@
 package main
 
-import "fmt"
-func change(a *[]int){
-	fmt.Println((*a)[0])
+import (
+	"fmt"
+	"strings"
+)
+
+type Animal struct {
+	name  string
+	food  string
+	move  string
+	sound string
+}
+
+//methods
+func (ani Animal) Eat() {
+	fmt.Println(ani.food)
+}
+func (ani Animal) Move() {
+	fmt.Println(ani.move)
+}
+func (ani Animal) Speak() {
+	fmt.Println(ani.sound)
+}
+func ProcessRes(animalName string, featureName string, animalMap map[string]Animal) {
+	animNameLower := strings.ToLower(animalName)
+	featureNameLower := strings.ToLower(featureName)
+	var ani Animal
+	switch animNameLower {
+	case "cow":
+		ani = animalMap["cow"]
+	case "snake":
+		ani = animalMap["snake"]
+	case "bird":
+		ani = animalMap["snake"]
+	}
+	switch featureNameLower {
+	case "eat":
+		ani.Eat()
+	case "move":
+		ani.Move()
+	case "speak":
+		ani.Speak()
+	}
 }
 func main() {
-	a:= []int{1,2,3,4,5,6,7}
-	change(&a)
-	fmt.Println(a)
+	var a, b string
+
+	cow := Animal{
+		name:  "cow",
+		food:  "grass",
+		move:  "walk",
+		sound: "moo",
+	}
+	bird := Animal{
+		name:  "bird",
+		food:  "worms",
+		move:  "fly",
+		sound: "peep",
+	}
+	snake := Animal{
+		name:  "snake",
+		food:  "mice",
+		move:  "slither",
+		sound: "hsss",
+	}
+	animalMap := map[string]Animal{
+		"cow":   cow,
+		"bird":  bird,
+		"snake": snake,
+	}
+	for {
+		fmt.Print("> ")
+		fmt.Scanf("%s %s", &a, &b)
+		ProcessRes(a, b, animalMap)
+	}
 }
